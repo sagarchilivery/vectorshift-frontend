2a53a8806bed931dbfcc31efd31e6a2da6ec4f90
Sagar <chiliverysagar@gmail.com>
2024-09-14 09:59:02 +0530

    Refactor App.js to set document title on page load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { PipelineToolbar } from "./toolbar";
 import { PipelineUI } from "./ui";
 import { SubmitButton } from "./submit";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "VectorShift | Sagar";
+  }, []);
   return (
     <div>
       <PipelineToolbar />
